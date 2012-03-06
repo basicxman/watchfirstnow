2f962d9504bf9d0501e6a686687e4e1a7de251ad
Andrew Horsman <minirobotics@gmail.com>
2012-03-05 22:06:59 -0500

    Parsed FMS tweets for Rebound Rumble.

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -13,17 +13,22 @@ data.sort! { |a, b| a["created_at"] <=> b["created_at"] }
 data.map! do |tweet|
   text = tweet["text"]
   {
-    :eventCode   => text.match(/#FRC([a-zA-Z0-9]+)\s/)[1],
-    :matchType   => text.match(/TY\s([A-Z]{1})/)[1],
-    :matchNumber => text.match(/MC\s([0-9]+)/)[1],
-    :redScore    => text.match(/RF\s([0-9]+)/)[1],
-    :blueScore   => text.match(/BF\s([0-9]+)/)[1],
-    :redTeams    => text.match(/RE\s([0-9]+)\s([0-9]+)\s([0-9]+)/)[1..-1].join("<br />"),
-    :blueTeams   => text.match(/BL\s([0-9]+)\s([0-9]+)\s([0-9]+)/)[1..-1].join("<br />"),
-    :redBonus    => text.match(/RB\s([0-9]+)/)[1],
-    :blueBonus   => text.match(/BB\s([0-9]+)/)[1],
-    :redPenalty  => text.match(/RP\s([0-9]+)/)[1],
-    :bluePenalty => text.match(/BP\s([0-9]+)/)[1]
+    :eventCode    => text.match(/#FRC([a-zA-Z0-9]+)\s/)[1],
+    :matchType    => text.match(/TY\s([A-Z]{1})/)[1],
+    :matchNumber  => text.match(/MC\s([0-9]+)/)[1],
+    :redScore     => text.match(/RF\s([0-9]+)/)[1],
+    :blueScore    => text.match(/BF\s([0-9]+)/)[1],
+    :redTeams     => text.match(/RA\s([0-9]+)\s([0-9]+)\s([0-9]+)/)[1..-1].join("<br />"),
+    :blueTeams    => text.match(/BA\s([0-9]+)\s([0-9]+)\s([0-9]+)/)[1..-1].join("<br />"),
+    :redBridge    => text.match(/RB\s([0-9]+)/)[1],
+    :blueBridge   => text.match(/BB\s([0-9]+)/)[1],
+    :redFouls     => text.match(/RFP\s([0-9]+)/)[1],
+    :blueFouls    => text.match(/BFP\s([0-9]+)/)[1],
+    :redHybrid    => text.match(/RHS\s([0-9]+)/)[1],
+    :blueHybrid   => text.match(/BHS\s([0-9]+)/)[1],
+    :redTeleop    => text.match(/RTS\s([0-9+])/)[1],
+    :blueTeleop   => text.match(/BTS\s([0-9+])/)[1],
+    :coopertition => text.match(/CP\s([0-9+])/)[1]
   }
 end
 
--- lib/twitter_scores.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=frcfms&count=100";
-
-  
-?>
